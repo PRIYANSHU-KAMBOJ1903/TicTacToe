d3c2a818cb34dc8463e96f527bfb795571653933
Priyanshu <priyanshukamboj1903@gmail.com>
2025-12-04 15:11:10 +0530

    some update

--- src/components/TicTacToe.jsx
+++ src/components/TicTacToe.jsx
@@ -19,7 +19,16 @@ const TicTacToe = () => {
             data[num]="o";
             setCount(++count);
         }
-
+    }
+    const checkWin=()=>{
+        if(data[0]===data[1]&&data[1]===data[2]&&data[2]!==""){
+            won(data);
+        }else if(data[3]===data[4]&&data[4]===data[5]&&data[5]!==""){
+            won(data);
+        }
+    }
+    const won=(winner)=>{
+        setLock(true);
     }
   return (
     <div className='container'>
